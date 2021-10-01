f8d71e3131b73467434b8dc3d9a4990e43b4b48b
Bram van Ballegooijen <99046330@mydavinci.nl>
2021-10-01 11:34:58 +0200

    added index.php / functies.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+
+    <header>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+    </header>
+
+    <body>
+        <?php
+            
+            include 'functies.php';
+
+        ?>
+    </body>
+
+</html> 
