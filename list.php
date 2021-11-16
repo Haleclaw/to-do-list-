99cecc7d29b13d4f3574d5d40505aed65fd8507c
Bram van Ballegooijen <99046330@mydavinci.nl>
2021-11-16 14:40:42 +0100

    styling list.php

--- list.php
+++ list.php
@@ -6,15 +6,13 @@
 
 
     <body>
-        <div class = "w3-card" style="width:20%">
+        <div class = "w3-card" style="width:20%; height:80%">
             <h3 class = "w3-center"><?php echo $listName ?></h3>
-            <button class = 'w3-btn w3-grey'type = "submit" name = "addTask">add task</button>
-            <button class = 'w3-btn w3-grey'type = "submit" name = "deleteList">delete list</button>
+                <div class = 'w3-center'>
+                    <button class = 'w3-btn w3-teal'type = "submit" name = "addTask">add task</button>
+                    <button class = 'w3-btn w3-teal'type = "submit" name = "deleteList">delete list</button>
+                </div>
         </div>
     </body>
-
-
-
-
 
 </html>
